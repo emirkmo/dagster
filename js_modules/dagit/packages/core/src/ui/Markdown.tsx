0d98e35bb2e2b404a037a3b865c8a6c51e444781
Isaac Hellendag <dish@elementl.com>
2023-03-31 13:07:51 -0500

    [ui] ReactMarkdown stories and tweaks

--- js_modules/dagit/packages/core/src/ui/Markdown.tsx
+++ js_modules/dagit/packages/core/src/ui/Markdown.tsx
@@ -2,6 +2,7 @@ import {Colors, FontFamily} from '@dagster-io/ui';
 import * as React from 'react';
 import ReactMarkdown from 'react-markdown';
 import rehypeHighlight from 'rehype-highlight';
+import rehypeSanitize, {defaultSchema} from 'rehype-sanitize';
 import gfm from 'remark-gfm';
 import styled from 'styled-components/macro';
 
@@ -14,7 +15,69 @@ interface Props {
 export const Markdown: React.FC<Props> = (props) => {
   return (
     <Container>
-      <ReactMarkdown remarkPlugins={[gfm]} rehypePlugins={[rehypeHighlight]} {...props} />
+      <ReactMarkdown
+        remarkPlugins={[gfm]}
+        rehypePlugins={[
+          rehypeHighlight,
+          [
+            rehypeSanitize,
+            {
+              ...defaultSchema,
+              attributes: {
+                ...defaultSchema.attributes,
+                span: [
+                  ...(defaultSchema.attributes?.span || []),
+                  // List of all allowed tokens:
+                  [
+                    'className',
+                    'hljs-addition',
+                    'hljs-attr',
+                    'hljs-attribute',
+                    'hljs-built_in',
+                    'hljs-bullet',
+                    'hljs-char',
+                    'hljs-code',
+                    'hljs-comment',
+                    'hljs-deletion',
+                    'hljs-doctag',
+                    'hljs-emphasis',
+                    'hljs-formula',
+                    'hljs-keyword',
+                    'hljs-link',
+                    'hljs-literal',
+                    'hljs-meta',
+                    'hljs-name',
+                    'hljs-number',
+                    'hljs-operator',
+                    'hljs-params',
+                    'hljs-property',
+                    'hljs-punctuation',
+                    'hljs-quote',
+                    'hljs-regexp',
+                    'hljs-section',
+                    'hljs-selector-attr',
+                    'hljs-selector-class',
+                    'hljs-selector-id',
+                    'hljs-selector-pseudo',
+                    'hljs-selector-tag',
+                    'hljs-string',
+                    'hljs-strong',
+                    'hljs-subst',
+                    'hljs-symbol',
+                    'hljs-tag',
+                    'hljs-template-tag',
+                    'hljs-template-variable',
+                    'hljs-title',
+                    'hljs-type',
+                    'hljs-variable',
+                  ],
+                ],
+              },
+            },
+          ],
+        ]}
+        {...props}
+      />
     </Container>
   );
 };
